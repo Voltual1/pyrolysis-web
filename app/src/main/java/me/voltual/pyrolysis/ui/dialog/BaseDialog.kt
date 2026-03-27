013bf107b6d39343e4e4583b295a60902230070a
Voltual1 <18206039173@163.com>
2026-03-27 19:56:04 +0800

    为从之前从Neo Store合并过来的一些忘记添加Header的文件添加Header说明代码来源

--- app/src/main/java/me/voltual/pyrolysis/ui/dialog/BaseDialog.kt
+++ app/src/main/java/me/voltual/pyrolysis/ui/dialog/BaseDialog.kt
@@ -1,3 +1,12 @@
+/*
+ * This file is adapted from Neo Store (https://github.com/NeoApplications/Neo-Store)
+ * Modified by Voltual to fit Pyrolysis architecture.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ */
+
 package me.voltual.pyrolysis.ui.dialog
 
 import androidx.compose.runtime.Composable
