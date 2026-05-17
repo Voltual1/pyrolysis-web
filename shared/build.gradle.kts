7dd10c0b8b2cbbb125287b5f38f2fac7d4990f9c
Voltual <3196570237@qq.com>
2026-05-18 05:53:07 +0800

    update

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -40,6 +40,7 @@ kotlin {
                 implementation(libs.ktor.client.core)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.json)
+                implementation(libs.ktor.client.logging)
                 implementation(libs.kotlinx.serialization.json)
                 implementation(libs.kotlinx.datetime)
             }
