a605e90ea05c65a08265b46eabf2c47eed2546c1
Voltual <3196570237@qq.com>
2026-05-18 03:54:39 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,13 @@
 // Root build.gradle.kts
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library).apply(false) 
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
@@ -15,9 +16,12 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://jitpack.io") }       
         maven { url = uri("$rootDir/ijkplayer-main") }
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
 rootProject.name = "Pyrolysis"
-include(":app")
+include(":shared")
+include(":android")
+include(":web")
 include(":DanmakuFlameMaster")
