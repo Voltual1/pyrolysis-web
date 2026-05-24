b8a868b66bc52d3f67ba715024c1413cad7bd1ff
Voltual1 <18206039173@163.com>
2026-05-24 12:58:44 +0800

    update

--- arsc/src/commonMain/kotlin/dev/rushii/arsc/ArscFile.kt
+++ arsc/src/commonMain/kotlin/dev/rushii/arsc/ArscFile.kt
@@ -30,8 +30,8 @@ public class ArscFile(public val packages: List<ArscPackage>) {
 
 		val finalBuffer = Buffer()
 		val header = ArscHeader(
-			ArscHeaderType.Table, 
-			ArscHeader.size().toUShort(), 
+			ArscHeaderType.Table, 
+			ArscHeader.size().toUShort(), 
 			(ArscHeader.size() + globalBuffer.size).toUInt()
 		)
 		ArscHeader.write(finalBuffer, header)
