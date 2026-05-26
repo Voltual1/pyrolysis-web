0ab9e34be155de3046ed853289208461eaa98859
Voltual <3196570237@qq.com>
2026-05-27 02:06:19 +0800

    update

--- ApkParser/src/commonTest/kotlin/me/voltual/apkparser/test/ApkParserTest.kt
+++ ApkParser/src/commonTest/kotlin/me/voltual/apkparser/test/ApkParserTest.kt
@@ -7,13 +7,18 @@ import me.voltual.apkparser.ApkParser
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-//class ApkParserTest {
+class ApkParserTest {
 
     @Test
     fun testParseRealApk() {
-        // 假设您的测试资源里有一个测试用的 apk
         val path = Path("src/commonTest/resources/test.apk")
-        if (!SystemFileSystem.exists(path)) return
+        
+        // 检查文件是否存在
+        if (!SystemFileSystem.exists(path)) {
+            println("【提示】未找到测试 APK 文件，路径: ${path.toString()}")
+            println("请将测试用的 APK 命名为 'test.apk' 并放置在 'src/commonTest/resources/' 目录下再进行完整解析测试。")
+            return // 依然需要结束当前测试方法，但在此之前已经完成了信息打印
+        }
 
         val source = SystemFileSystem.source(path).buffered()
         val parser = ApkParser(source)
