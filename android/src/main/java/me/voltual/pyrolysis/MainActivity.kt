7dea76c30a4e7975d4df1de7b5d0c3756c06c56d
Voltual <3196570237@qq.com>
2026-05-28 23:53:47 +0800

    update

--- android/src/main/java/me/voltual/pyrolysis/MainActivity.kt
+++ android/src/main/java/me/voltual/pyrolysis/MainActivity.kt
@@ -10,6 +10,7 @@ package me.voltual.pyrolysis
 
 import android.content.Context
 import android.content.Intent
+import android.app.ActivityOptions
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
