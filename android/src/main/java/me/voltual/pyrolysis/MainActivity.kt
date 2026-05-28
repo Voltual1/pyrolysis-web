b10682d451fc138086f25020c78c60d535d6da83
Voltual <3196570237@qq.com>
2026-05-29 02:33:00 +0800

    update

--- android/src/main/java/me/voltual/pyrolysis/MainActivity.kt
+++ android/src/main/java/me/voltual/pyrolysis/MainActivity.kt
@@ -27,7 +27,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
