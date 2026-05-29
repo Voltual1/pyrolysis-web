5c1a08b3f96251b3b4c4389b6dda2e8c532ace6b
Voltual1 <18206039173@163.com>
2026-05-29 19:52:21 +0800

    update

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -53,7 +53,6 @@ kotlin {
                 implementation(libs.ktor.client.logging)
                 implementation(libs.kotlinx.serialization.json)
                 implementation(libs.koin.android.compose)
-                implementation(libs.compose.material.icons.extended)
                 implementation(libs.kotlinx.coroutines.core)
                 implementation(libs.kotlinx.datetime)
                 implementation(libs.koin.core)
