a40b08fb7350d81d5e8b907f6eddf9f16e998fea
Voltual1 <18206039173@163.com>
2026-05-30 23:03:41 +0800

    update

--- shared/src/commonMain/kotlin/me/voltual/pyrolysis/ui/player/PlayerScreen.kt
+++
@@ -1,9 +0,0 @@
-package me.voltual.pyrolysis.ui.player
-
-import androidx.compose.runtime.Composable
-
-@Composable
-expect fun PlayerScreen(
-    viewModel: PlayerViewModel, 
-    onBack: () -> Unit
-)
