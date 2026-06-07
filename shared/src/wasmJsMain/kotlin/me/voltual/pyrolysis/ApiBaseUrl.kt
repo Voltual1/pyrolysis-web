cc82feff87920a7e1efb9af70e7d2a9f1d7f1ead
Voltual1 <18206039173@163.com>
2026-06-07 17:15:26 +0800

    update

--- shared/src/wasmJsMain/kotlin/me/voltual/pyrolysis/ApiBaseUrl.kt
+++ shared/src/wasmJsMain/kotlin/me/voltual/pyrolysis/ApiBaseUrl.kt
@@ -1,5 +1,5 @@
 package me.voltual.pyrolysis
 
-// 因为是同域下的反代，直接使用 /functions/api/ 前缀
-actual val ApiBaseUrl: String = "/functions/api/"
-actual val WanyueyunUploadApiBaseUrl: String = "/functions/upload/"
+// 因为是同域下的反代，直接
+actual val ApiBaseUrl: String = ""
+actual val WanyueyunUploadApiBaseUrl: String = ""
