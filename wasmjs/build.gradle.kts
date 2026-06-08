a586e5232deb14a3f2700c93025fa1bfadffd31a
Voltual1 <18206039173@163.com>
2026-06-08 19:53:11 +0800

    update

--- wasmjs/build.gradle.kts
+++ wasmjs/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
             implementation(libs.compose.ui)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
+            implementation(libs.koin.compose)
             implementation(libs.koin.core)            
             // 修复：补齐 Compose 资源加载核心依赖，使 Wasm 壳工程能解析 FontResource
             implementation(libs.components.resources)
